4dd2c3d16fd1e8c0ceb7adea41111394131722c3
meferdzo <rok.ferjancic.20@gmail.com>
2018-01-13 18:08:14 +0100

    Added round slider, rearrange the code.

--- js/circularslider.js
+++ js/circularslider.js
@@ -1,23 +1,20 @@
-var canvas = document.getElementById('mycanvas');
-var angle = 0;
-
-
-var ctx = canvas.getContext('2d');
+let canvas = document.getElementById('mycanvas');
+let angle = 0;
 
 // making sure that canvas is connected to HTML
-if(canvas.getContext){
-    var ctx = canvas.getContext('2d');
+if (canvas.getContext) {
+  let ctx = canvas.getContext('2d');
 
-    // prepairing draw function and parameters
-    function drawbase(){
-      for(var i = 0; i < 5; i++){
+  // prepairing draw function and parameters
+  function drawbase() {
+    for (let i = 0; i < 5; i++) {
       //setting parameters for circle
-      var centerx = canvas.width / 2;
-      var centery = canvas.height/ 2;
-      var offset = 1.5 * Math.PI;
-      var radius = 50 * (i + 1);
+      let centerx = canvas.width / 2;
+      let centery = canvas.height / 2;
+      let offset = 1.5 * Math.PI;
+      let radius = 50 * (i + 1);
 
-      //draw circle stlyes
+      //draw base circle stlyes
       ctx.beginPath();
       ctx.arc(centerx, centery, radius, 1.5 * Math.PI, 3.5 * Math.PI, false);
       ctx.lineWidth = 35;
@@ -26,10 +23,110 @@ if(canvas.getContext){
       ctx.strokeStyle = '#bcbcbc';
       ctx.stroke();
       ctx.closePath();
-      }
     }
-    drawbase();
+  }
+  drawbase();
+
+  // object options
+  let options = [{
+      container: 'slider',
+      color: '#51A5C3',
+      maxValue: '3000',
+      minValue: '0',
+      step: '1',
+      startValue: '250',
+      description: 'transportation',
+      radius: '250',
+      part: -0.5 * Math.PI
+    },
+    {
+      container: 'slider',
+      color: '#224160',
+      maxValue: '3000',
+      minValue: '0',
+      step: '1',
+      startValue: '200',
+      description: 'food',
+      radius: '250',
+      part: -0.5 * Math.PI
+    },
+    {
+      container: 'slider',
+      color: '#ab3636',
+      maxValue: '3000',
+      minValue: '0',
+      step: '1',
+      startValue: '150',
+      description: 'insurance',
+      radius: '250',
+      part: -0.5 * Math.PI
+    },
+    {
+      container: 'slider',
+      color: '#ff9900',
+      maxValue: '3000',
+      minValue: '0',
+      step: '1',
+      startValue: '150',
+      description: 'entertainment',
+      radius: '100',
+      part: -0.5 * Math.PI
+    },
+    {
+      container: 'slider',
+      color: '#ff9900',
+      maxValue: '3000',
+      minValue: '0',
+      step: '1',
+      startValue: '150',
+      description: 'healhtcare',
+      radius: '50',
+      part: -0.5 * Math.PI
+    },
+  ]
+
+  function setCanvas() {
+    let canvasArray = [];
+    let uniqueCanvases = [];
+
+    for (let j = 0; j < options.length; j++) {
+      if (options[j].container != 'slider')
+        canvasArray.push(options[j].container);
+    }
+  }
+
+  /*function duplicates(canvasArray){
+
+  }*/
+
+  function drawSlider() {
+    let canvas = document.getElementById('mycanvas');
+    let ctx = canvas.getContext('2d');
+    let centerx = canvas.width / 2;
+    let centery = canvas.height / 2;
+
+    ctx.beginPath();
+    ctx.globalAlpha = 1;
+    ctx.arc(centerx, centery, 25, -0.5 * Math.PI, 2 * Math.PI, false);
+    ctx.fillStyle = '#8f9399';
+    ctx.fill();
+    ctx.setLineDash([0, 0]);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'red'
+    ctx.stroke();
+    ctx.closePath();
+
+    console.log("Slider working!")
+  }
+  /*
+  function mustClear(){
+    let canvas = document.getElementById('mycanvas');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }*/
+
+  drawSlider();
+
+  setCanvas();
 
 }
-
-console.log(canvas);
