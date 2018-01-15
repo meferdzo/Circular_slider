e92aa1f9633d38701ab538603a922d2205a0d6c8
meferdzo <rok.ferjancic.20@gmail.com>
2018-01-15 08:20:23 +0100

    Instances of knobs are now arranged from center.

--- js/circularslider.js
+++ js/circularslider.js
@@ -25,108 +25,90 @@ if (canvas.getContext) {
       ctx.closePath();
     }
   }
+
+  function drawSlider() {
+    for (let i = 0; i < 5; i++) {
+      let canvas = document.getElementById('mycanvas');
+      let ctx = canvas.getContext('2d');
+      let offsetY = 50 * (i + 1);
+      let centerx = canvas.width / 2;
+      let centery = canvas.height / 2 - offsetY;
+
+      ctx.beginPath();
+      ctx.globalAlpha = 1;
+      ctx.arc(centerx, centery, 20, -0.5 * Math.PI, 2 * Math.PI, false);
+      ctx.fillStyle = '#8f9399';
+      ctx.fill();
+      ctx.setLineDash([0, 0]);
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'grey';
+      ctx.stroke();
+      ctx.closePath();
+
+    }
+    console.log("Slider working!")
+  }
+
   drawbase();
+  drawSlider();
 
   // object options
   let options = [{
       container: 'slider',
       color: '#51A5C3',
-      maxValue: '3000',
-      minValue: '0',
-      step: '1',
-      startValue: '250',
+      max: 3000,
+      min: 0,
+      step: 1,
+      startValue: 250,
       description: 'transportation',
-      radius: '250',
+      radius: 250,
       part: -0.5 * Math.PI
     },
     {
       container: 'slider',
       color: '#224160',
-      maxValue: '3000',
-      minValue: '0',
-      step: '1',
-      startValue: '200',
+      max: 3000,
+      min: 0,
+      step: 1,
+      startValue: 200,
       description: 'food',
-      radius: '250',
+      radius: 250,
       part: -0.5 * Math.PI
     },
     {
       container: 'slider',
       color: '#ab3636',
-      maxValue: '3000',
-      minValue: '0',
-      step: '1',
-      startValue: '150',
+      max: 3000,
+      min: 0,
+      step: 1,
+      startValue: 150,
       description: 'insurance',
-      radius: '250',
+      radius: 250,
       part: -0.5 * Math.PI
     },
     {
       container: 'slider',
       color: '#ff9900',
-      maxValue: '3000',
-      minValue: '0',
-      step: '1',
-      startValue: '150',
+      max: 3000,
+      min: 0,
+      step: 1,
+      startValue: 150,
       description: 'entertainment',
-      radius: '100',
+      radius: 100,
       part: -0.5 * Math.PI
     },
     {
       container: 'slider',
       color: '#ff9900',
-      maxValue: '3000',
-      minValue: '0',
-      step: '1',
-      startValue: '150',
+      max: 3000,
+      min: 0,
+      step: 1,
+      startValue: 150,
       description: 'healhtcare',
-      radius: '50',
+      radius: 50,
       part: -0.5 * Math.PI
     },
   ]
 
-  function setCanvas() {
-    let canvasArray = [];
-    let uniqueCanvases = [];
-
-    for (let j = 0; j < options.length; j++) {
-      if (options[j].container != 'slider')
-        canvasArray.push(options[j].container);
-    }
-  }
-
-  /*function duplicates(canvasArray){
-
-  }*/
-
-  function drawSlider() {
-    let canvas = document.getElementById('mycanvas');
-    let ctx = canvas.getContext('2d');
-    let centerx = canvas.width / 2;
-    let centery = canvas.height / 2;
-
-    ctx.beginPath();
-    ctx.globalAlpha = 1;
-    ctx.arc(centerx, centery, 25, -0.5 * Math.PI, 2 * Math.PI, false);
-    ctx.fillStyle = '#8f9399';
-    ctx.fill();
-    ctx.setLineDash([0, 0]);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = 'red'
-    ctx.stroke();
-    ctx.closePath();
-
-    console.log("Slider working!")
-  }
-  /*
-  function mustClear(){
-    let canvas = document.getElementById('mycanvas');
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }*/
-
-  drawSlider();
-
-  setCanvas();
 
 }
